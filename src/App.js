a332a27a6e22714c2fcef18fe7f3561328b5dd0b
andre09999 <andre09999@live.com>
2023-04-25 15:05:33 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/footer';
 import Home from './pages/home';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
      <Header/>
      <Home/>
+     <Footer/>
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+function Footer() {
+  return (
+
+      <footer >
+       <p>Criado e desenvolvido por<a className='footerA' href='https://portifoiliowebandre.netlify.app/' target='_blank' rel="noreferrer"> Andre Luis</a> </p>
+      </footer>
+
+  );
+}
+
+export default Footer;
