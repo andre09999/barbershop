6cd3469dfe21e9d2b952dd8b3ea075ee8edfbf6d
andre09999 <andre09999@live.com>
2023-04-29 09:30:02 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { Route, Switch, Redirect } from 'react-router-dom';
-import { useContext } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/footer';
 import Home from './pages/home';
 import Agendamento from './pages/agendamento';
-import MyContext from '../src/context/Context';
 
 function App() {
 
